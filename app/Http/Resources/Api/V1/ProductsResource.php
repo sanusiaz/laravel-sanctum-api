7d398f6acc4d19846f659640df10ab3f96bac909
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2023-01-02 12:47:04 +0100

    Added id in product resource

--- app/Http/Resources/Api/V1/ProductsResource.php
+++ app/Http/Resources/Api/V1/ProductsResource.php
@@ -15,6 +15,7 @@ class ProductsResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'description' => $this->description,
