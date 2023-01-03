ee9de6023a420ad2d81dc9292bf241486c40a1cd
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2023-01-03 08:44:46 +0100

    Users Login, logout and register api request

---
+++ app/Http/Controllers/Api/V1/Auth/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Api\V1\StoreUserRequest;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    public function register( StoreUserRequest $request )
+    {
+        // validate request
+        $request->validated();
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        return response([
+            'data' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+
+    public function logout() 
+    {
+        auth()->user()->tokens()->delete();
+
+        return response([
+            'message' => 'logout successful'
+        ], 201);
+    }
+
+    public function login( Request $request )
+    {
+        $this->validate($request, [
+            'email' => ['email', 'required', 'max:255'],
+            'password' => ['required', 'max:255']
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if ( $user !== null )  
+        {
+            if ( Hash::check($request->password, $user->password) ) {
+                // create new token 
+                $token = $user->createToken('myapptoken')->plainTextToken;
+
+                return response([
+                    'data' => [
+                        'email' => $request->email,
+                        'token' => $token
+                    ]
+                ], 201);
+            }
+        }
+
+        return response([
+            'message' => 'Invalid Credentials'
+        ], 501);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\UserController;
 use App\Http\Controllers\Api\V1\ProductController;
+use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,17 +10,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::group([
     'prefix' => 'v1',
     'namespace' => 'App\Http\Controllers\Api\V1'
 ], function () {
-
+    
+    
     Route::middleware(['auth:sanctum'])->post('/products', [ProductController::class, 'store'])
         ->name('products.store');
     Route::middleware(['auth:sanctum'])->post('/products/{product}', [ProductController::class, 'update'])
         ->name('products.update');
-    Route::middleware(['auth:sanctum'])->delete('/products/{products}', [ProductController::class, 'destroy'])
+        Route::middleware(['auth:sanctum'])->delete('/products/{products}', [ProductController::class, 'destroy'])
         ->name('products.destroy');
+        
+    
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+    Route::middleware(['auth:sanctum'])->post('/logout', [UserController::class, 'logout']);
+
     Route::apiResource('/products', ProductController::class);
 
    
